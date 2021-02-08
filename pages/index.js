bef3895ed17be122bad122925156c2bdac7d0a49
Andy Kay <andykay.me@gmail.com>
2021-02-08 14:00:27 -0500

    auth api url

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ const Home = (props) => {
   return (
     <div>
       <img src="/fitfam-blue@2x.png" alt="FitFam" height="25px" />
+
+      <a href={`${process.env.AUTH_API_URL}/facebook`}>Sign Up With Facebook</a>
       <UserList />
     </div>
   );
