81f9dab10feb9cd8be750d11f8db170199aba0c8
Andy Kay <andykay.me@gmail.com>
2021-02-15 07:54:12 -0500

    remove tailwindcss and add chakra-ui

---
+++ components/Layout.js
@@ -0,0 +1,45 @@
+import { Flex, Box, Image, Link } from "@chakra-ui/react";
+import Head from "next/head";
+import NextLink from "next/link";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Head>
+        <title>FitFam - Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Box height="100vh">
+        <Flex
+          px={6}
+          py={4}
+          borderBottom="2px solid"
+          borderColor="#f8f8f8"
+          align="center"
+          justify="space-between"
+          backgroundColor="white"
+        >
+          <Box>
+            <NextLink href="/">
+              <Link>
+                <Image
+                  src="/fitfam-blue@2x.png"
+                  alt="FitFam"
+                  height="25px"
+                  ignoreFallback
+                />
+              </Link>
+            </NextLink>
+          </Box>
+        </Flex>
+
+        <Box px={6} py={4}>
+          {children}
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,6 @@ import UserList from "../components/UserList";
 const Home = (props) => {
   return (
     <div>
-      <img src="/fitfam-blue@2x.png" alt="FitFam" className="h-25" />
-
       <a href={`${process.env.NEXT_PUBLIC_AUTH_API_URL}/facebook`}>
         Sign Up With Facebook
       </a>
