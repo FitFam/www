14cea0024192cef02304cd90b336aa8edde6bc05
Andy Kay <andykay.me@gmail.com>
2021-03-04 03:50:50 -0500

    remove UserList for now

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import withApollo from "../lib/apolloClient";
-import UserList, { ALL_USERS_QUERY } from "../components/UserList";
 import { initializeApollo, addApolloState } from "../lib/apolloClient";
 import SingUpForm from "../components/SignUpForm";
 
@@ -7,21 +6,8 @@ const Home = (props) => {
   return (
     <div>
       <SingUpForm />
-      <UserList />
     </div>
   );
 };
-
-export async function getServerSideProps() {
-  const apolloClient = initializeApollo();
-
-  await apolloClient.query({
-    query: ALL_USERS_QUERY,
-  });
-
-  return addApolloState(apolloClient, {
-    props: {},
-  });
-}
 
 export default Home;
