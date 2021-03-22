b55b09eaaf550fb7edb96a97cda18ae0c4d75123
Andy Kay <andykay.me@gmail.com>
2021-03-22 07:03:19 -0400

    user profile layout

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,6 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, Avatar, Heading, Text } from "@chakra-ui/react";
 
 const GET_USER = gql`
   query UserProfile($username: String!) {
@@ -26,8 +26,15 @@ function UserProfile() {
   if (data && data.user) {
     return (
       <Box>
-        <Box>{data.user.name}</Box>
-        <Box>{data.user.bio}</Box>
+        <Flex mb={4}>
+          <Avatar src={data.user.avatar} mr={3} />
+          <Box>
+            <Heading size="md">{data.user.username}</Heading>
+            <Box>{data.user.name}</Box>
+          </Box>
+        </Flex>
+
+        <Text>{data.user.bio}</Text>
       </Box>
     );
   }
